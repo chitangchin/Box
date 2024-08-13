34c395ac53b0672a2a0026771b653870695810a5
chitangchin <chitangb.chin@gmail.com>
2024-08-13 16:41:44 -0400

    OOP Mock Bank Account

--- Practice/PracticeCollection/OOP/MockBankAccount.cs
+++ Practice/PracticeCollection/OOP/MockBankAccount.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PracticeCollection.OOP
+namespace PracticeCollection.OOPMockBankAcc
 {
-    internal class MockBankAccount
+    public class MockBankAccount(string name, decimal initialBalance)
     {
+        private static int s_accountNumberSeed = 1234567890;
+        public string Number { get; } = s_accountNumberSeed.ToString();
+        public string Owner { get; set; } = name;
+        public decimal Balance { get; } = initialBalance;
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
     }
 }
--- Practice/PracticeCollectionTest/OOP/MockBankAccountTest.cs
+++ Practice/PracticeCollectionTest/OOP/MockBankAccountTest.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PracticeCollectionTest.OOP
+namespace PracticeCollectionTest.OOP.MockBankAccount
 {
-    public class MockBankAccountTest
+    using PracticeCollection.OOPMockBankAcc;
+    public static class MockBankAccountTests
     {
+        [Test]
+        public static void Tests()
+        {
+            var account = new MockBankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
     }
 }
