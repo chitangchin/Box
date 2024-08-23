1b52aeb0b75f7950c148838867c80fea5010279e
chitangchin <chitangb.chin@gmail.com>
2024-08-23 08:31:24 -0400

    SelectionSort

---
+++ Practice/PracticeCollection/Sorting/SelectionSort.cs
@@ -0,0 +1,24 @@
+﻿namespace PracticeCollection.Sorting.SelectionSort
+{
+    public class SolutionClass
+    {
+        public static int[] SelectionSort(int[] nums)
+        {
+            //Iterate through the array
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                //Declare an int variable smallest assign it to i
+                int smallest = i;
+                //iterate through the all elements after i
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    //initially Check if nums[j] is smaller than our i, then reassign. check every el if smaller than smallest and redefine index;
+                    if (nums[j] < nums[smallest]) smallest = j;
+                }
+                //Swap the smallest value with our current i
+                (nums[i], nums[smallest]) = (nums[smallest], nums[i]);
+            }
+            return nums;
+        }
+    }
+}
---
+++ Practice/PracticeCollectionTest/Sorting/SelectionSortTest.cs
@@ -0,0 +1,18 @@
+﻿using PracticeCollection.Sorting.SelectionSort;
+
+namespace PracticeCollectionTest.Sorting;
+public class SelectionSortTest
+{
+    private static readonly int[] expected = [1, 2, 3];
+    private static readonly int[] expected2 = [1, 2, 3, 4, 6, 8];
+    [Test]
+    public void Test()
+    {
+        Assert.That(SolutionClass.SelectionSort([1,3,2]), Is.EqualTo(expected));
+    }
+    [Test]
+    public void Test1()
+    {
+        Assert.That(SolutionClass.SelectionSort([1, 8, 6, 2, 3, 4]), Is.EqualTo(expected2));
+    }
+}
