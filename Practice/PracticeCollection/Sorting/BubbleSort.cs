ebcf82a40f696b029eef3b7311bfb7bf3c88cfd7
chitangchin <chitangb.chin@gmail.com>
2024-08-23 08:15:52 -0400

    Bubble Sort!

---
+++ Practice/PracticeCollection/Sorting/BubbleSort.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticeCollection.Sorting.BubbleSort
+{
+    public class SolutionClass
+    {
+        public static int[] BubbleSort(int[] nums)
+        {
+            //Double loop where we check j and j+1 and swap if lower then we iterate againg
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        (nums[j], nums[j+1]) = (nums[j+1], nums[j]);
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ Practice/PracticeCollectionTest/Sorting/BubbleSortTest.cs
@@ -0,0 +1,21 @@
+﻿using PracticeCollection.Sorting.BubbleSort;
+
+namespace PracticeCollectionTest.Sorting
+{
+    public class BubbleSortTest
+    {
+        private static readonly int[] expected = new int[] { 1, 2, 3 };
+        private static readonly int[] expectedArray = new int[] { 1, 2, 3, 4, 6, 8 };
+
+        [Test]
+        public void Test()
+        {
+            Assert.That(SolutionClass.BubbleSort([2, 3, 1]), Is.EqualTo(expected));
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.BubbleSort([4,1,2,6,3,8]), Is.EqualTo(expectedArray));
+        }
+    }
+}
